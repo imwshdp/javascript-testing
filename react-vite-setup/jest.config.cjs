5f8ab70bef490eff43b4c9c3cbea4bcf093c45ea
imwshdp <im.wshdp@gmail.com>
2024-04-30 23:08:21 +0300

    release: tests env vite setup

---
+++ react-vite-setup/jest.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+module.exports = {
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.{js,jsx}'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
+	transform: {
+		'^.+\\.(js|jsx)$': 'babel-jest'
+	},
+	moduleFileExtensions: ['js', 'jsx'],
+	moduleNameMapper: {
+		'^.+\\.svg$': 'jest-svg-transformer',
+		'^.+\\.css$': 'identity-obj-proxy'
+	}
+};
